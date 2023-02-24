e32c5c8da883cb40e5394bc249574d853c87e099
Jacobtkristensen <115540527+Jacobtkristensen@users.noreply.github.com>
2023-02-24 14:46:39 +0100

    Initial commit

---
+++ src/main/java/com/example/madspilde/MadspildeApplication.java
@@ -0,0 +1,13 @@
+package com.example.madspilde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadspildeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadspildeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/madspilde/MadspildeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.madspilde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadspildeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
